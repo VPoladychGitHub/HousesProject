90af74bdc141e2889a4111fd332f3875b4bdeed8
VPoladychGitHub <vpoladych@ukr.net>
2022-11-09 16:00:59 +0200

    Добавьте файлы проекта.

---
+++ House.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HousesProject
+{
+    internal class House
+    {
+        [Required]
+        private string adress;
+        public string? Nickname { get; set; }
+        public string Adress
+        {
+            get => adress;
+            init
+            {
+                setAdress(value);
+            }
+        }
+
+        public House(string adress)
+        {
+            this.adress = adress;
+        }
+        public House()
+        {
+            this.adress = "Unknown";
+        }
+
+        public void setAdress(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return;
+            }
+            if (value.Length <= 5)
+            {
+                return;
+            }
+            adress = value;
+        }
+        public override string ToString()
+        {
+            if (!string.IsNullOrWhiteSpace(Nickname))
+            {
+                return $"{Nickname}"; ;
+            }
+            return $"{adress}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is House house)
+            {
+                return house.Adress == Adress;
+            }
+            return false;
+        }
+
+         public override int GetHashCode() => HashCode.Combine(Adress);
+
+        public static bool operator == (House h, House h2) => h.Equals(h2);
+
+        public static bool operator !=(House h, House h2) => !h.Equals(h2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using HousesProject;
+using System;
+
+/*Створити клас дім (House), який
+
+- 65: Має адресу, яку обов'язково потрібно задати при створенні, можна читати, але не можна змінити
+
+- +5: Має можливість змінити адресу за допомогою окремого методу. Метод має перевіряти, що адреса довше за 5 символів
+
+- +5: Має ім'я (nickname), що можна читати та змінювати без обмежень
+
+- +10: Якщо викликати на екземплярі класу ToString, якщо у дому є ім'я - повинно відображатися ім'я, якщо ні - то адреса
+
+- +15: При порівнянні двох домів повинна враховуватися лише адреса*/
+Console.WriteLine("===========   house1 =========================");
+var house1 = new House
+{
+    Adress = "kharkiv prosp Nauki house 18"
+};
+Console.WriteLine(house1);
+house1.Nickname = "nickname House";
+Console.WriteLine(house1);
+Console.WriteLine(house1.Adress + "   " + house1.Nickname);
+
+Console.WriteLine("===========   house2 =========================");
+var house2 = new House
+{
+    Adress = "kharkiv street Symska house 118"
+};
+house2.Nickname = "nickname House 2";
+Console.WriteLine(house2.Adress + "   " + house2.Nickname);
+Console.WriteLine(house2);
+
+Console.WriteLine("===========   !Equals =========================");
+Console.WriteLine(house1.Equals(house2));
+Console.WriteLine(house1 == house2);
+Console.WriteLine("===========   Equals =========================");
+house2.setAdress("kharkiv prosp Nauki house 18");
+Console.WriteLine(house1.Equals(house2));
+Console.WriteLine(house1 == house2);
